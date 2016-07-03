39f168900c8d35c2f760690509b1f2aaecbd7331
PattyDePuh <pattydpuh@gmail.com>
2016-07-03 17:24:26 +0200

    Semikolon ...

--- show/js/mathjax_border_fix.js
+++ show/js/mathjax_border_fix.js
@@ -2,5 +2,5 @@ setTimeout('start_fix()', 1000);
 
 function start_fix(){
 	$("span").css("border-color", "#FFFFFF");
-	$("span").css("border-left-width","0.000em")
+	$("span").css("border-left-width","0.000em");
 	}
